aa206a587c15def2a4cf9b249182b4d3524e18a8
mytionBr <matheuss.jesus2017@gmail.com>
2021-12-27 16:17:55 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import eventRoute from './routes/event.route.js'
+import cors from 'cors'
+
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+// routes
 app.use('/api/events', eventRoute)
 
 export default app;
