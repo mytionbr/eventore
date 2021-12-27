da23d6c5bad0393cd48b6d82b2c74e5aa3c7d7bc
mytionBr <matheuss.jesus2017@gmail.com>
2021-12-27 13:42:54 -0300

    add database script

---
+++ src/database/pool.js
@@ -0,0 +1,21 @@
+import pg from 'pg'
+const { Pool } = pg;
+
+class PoolClass {
+    _pool = null;
+
+    connect(option){
+        this._pool = new Pool(option)
+        return this._pool.query("SELECT 1+1 ;")
+    }
+
+    query(sql, values){
+        return this._pool.query(sql,values)
+    }
+
+    close(){
+        return this._pool.end()
+    }
+}
+
+export default new PoolClass()
--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 import app from './app.js';
 import config from './config/config.js';
 
+pool
+  .connect(config.dbDetails)
+  .then(()=>{
+    console.log('Connected database')
+
+    app.listen(config.PORT, ()=>{
+      console.log('Server is running')
+    })
+  })
+  .catch((err)=>{
+    console.log(`Error: ${err.message}`)
+  })
+
 app.listen(config.port, () => {
   console.log(`Server is running at ${config.port}`);
 });
