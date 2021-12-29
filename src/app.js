d1c7d454f4146d037ec5c86b4cb3f9ecadf7ce35
mytionBr <matheuss.jesus2017@gmail.com>
2021-12-29 15:11:30 -0300

    doing the frontend build

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import userRoute from './routes/user.router.js'
 import attendeeRoute from './routes/attendee.router.js'
 import authRoute from './routes/auth.router.js'
 import cors from 'cors'
+import path from 'path';
 
 const app = express();
 
@@ -16,5 +17,12 @@ app.use('/api/events', eventRoute);
 app.use('/api/users', userRoute);
 app.use('/api/attendees', attendeeRoute);
 app.use('/api/auth', authRoute)
+
+const __dirname = path.resolve()
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
+);
 
 export default app;
